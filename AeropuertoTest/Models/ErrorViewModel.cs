44d743dfb8ec417f6f0347ee9ad4b934add10836
Jesus Albeiro <albeiro1989@gmail.com>
2021-08-12 16:29:18 -0500

    Start proyect Aeropuerto, asp vmc

---
+++ AeropuertoTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AeropuertoTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
